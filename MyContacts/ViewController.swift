d90ed70872ec8d5455e91afed8a8c3b5098c5f7c
Kenneth Lee <KennethVLee@gmail.com>
2019-11-22 10:56:10 -0500

    Initial Commit

---
+++ MyContacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyContacts
+//
+//  Created by Lee, Kenneth Van on 11/22/19.
+//  Copyright © 2019 Lee, Kenneth Van. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
